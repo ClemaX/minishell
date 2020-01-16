ead25487f26950f8335b2e5160f67837f6340242
Clément Hamada <clementhamada@protonmail.com>
2020-01-16 18:28:10 +0100

    Keep arguments when waiting for input

--- srcs/command/command.c
+++ srcs/command/command.c
@@ -6,7 +6,7 @@
 /*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/01/12 04:45:30 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2020/01/16 15:43:31 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/01/16 18:20:28 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -26,14 +26,14 @@ t_status		cmd_parse(t_cmd *cmd, char *line, t_status status)
 	static t_line	*arg = NULL;
 	static t_line	*args = NULL;
 
-	status &= ~B_SLASH;
-	if (!(status & QUOTE))
+	if (!(status & (QUOTE | B_SLASH)))
 	{
 		line_clr(&args);
 		free(cmd->av);
 		cmd->ac = 0;
 		cmd->av = NULL;
 	}
+	status &= ~B_SLASH;
 	while (*line)
 	{
 		while (ft_isspace(*line))
@@ -47,6 +47,7 @@ t_status		cmd_parse(t_cmd *cmd, char *line, t_status status)
 			cmd->ac++;
 		}
 	}
-	args_export(cmd, args);
+	if (!(status & (QUOTE | B_SLASH)))
+		args_export(cmd, args);
 	return (status);
 }
