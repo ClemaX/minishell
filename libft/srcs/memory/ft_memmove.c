be038eb8f7464221c8e12714ec4f27d7770145f3
Clément Hamada <chamada@student.le-101.fr>
2020-01-17 14:44:59 +0100

    Remove t_size

--- libft/srcs/memory/ft_memmove.c
+++ libft/srcs/memory/ft_memmove.c
@@ -13,9 +13,9 @@
 
 #include <libft.h>
 
-void	*ft_memmove(void *dst, const void *src, t_size len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
-	t_size i;
+	size_t i;
 
 	i = 0;
 	if (src > dst)
