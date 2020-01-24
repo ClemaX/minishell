f34fb6e3b6249e18d8add4263f36b086ca50df98
plamtenz <plamtenz@student.le-101.fr>
2020-01-24 15:21:12 +0100

    .

--- includes/execution.h
+++ includes/execution.h
@@ -1,15 +1,15 @@
 /* ************************************************************************** */
-/*                                                          LE - /            */
-/*                                                              /             */
-/*   execution.h                                      .::    .:/ .      .::   */
-/*                                                 +:+:+   +:    +:  +:+:+    */
-/*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
-/*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2020/01/20 08:40:28 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2020/01/21 11:26:41 by chamada     ###    #+. /#+    ###.fr     */
-/*                                                         /                  */
-/*                                                        /                   */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execution.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plamtenz <plamtenz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/20 08:40:28 by chamada           #+#    #+#             */
+/*   Updated: 2020/01/23 19:19:39 by plamtenz         ###   ########.fr       */
+/*                                                                            */
 /* ************************************************************************** */
+
 
 #ifndef EXECUTION_H
 # define EXECUTION_H
@@ -18,11 +18,11 @@
 #include <map.h>
 #include <parser.h>
 
-void	simple_cmd(t_node *node, t_map *env, char *name, int (exec)(char *,
+int		simple_cmd(t_node *node, t_map *env, char *name, int (exec)(char *,
 	t_cmd *));
 void	cmd_line_execution(t_node *node, t_map *env, char *name);
-void	redirection(t_node *node, char *data, char std, t_map *env, char *name);
-void	ft_pipe(t_node *node, t_map *env, char *name);
+int		redirection(t_node *node, char *data, char std, t_map *env, char *name);
+int		ft_pipe(t_node *node, t_map *env, char *name);
 int		ft_execve_f(char *exec, t_cmd *cmd);
 int		ft_execve(char *exec, t_cmd *cmd);
 
