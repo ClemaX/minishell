73fe2a6f2b663b65901067b408955a74c5a52872
Clément Hamada <clementhamada@protonmail.com>
2020-01-26 19:03:36 +0100

    Fix unallocated pointer

--- srcs/prompt/line_utils.c
+++ srcs/prompt/line_utils.c
@@ -6,7 +6,7 @@
 /*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/01/25 20:55:47 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2020/01/25 21:50:38 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/01/26 19:01:59 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -14,6 +14,16 @@
 #include <line.h>
 #include <libft.h>
 #include <stdlib.h>
+
+static char	*char_dup(char c)
+{
+	char	*res;
+
+	if (!(res = malloc(sizeof(char))))
+		return (NULL);
+	*res = c;
+	return (res);
+}
 
 void	line_insert_at(t_line **line, int pos, char *str)
 {
@@ -26,7 +36,7 @@ void	line_insert_at(t_line **line, int pos, char *str)
 	if (!*line || !(len = line_len(*line))
 	|| (i = len - pos) == 0)
 		while (*str)
-			line_add(line, str++, 1);
+			line_add(line, char_dup(str++), 1);
 	else
 	{
 		start = *line;
@@ -34,7 +44,7 @@ void	line_insert_at(t_line **line, int pos, char *str)
 			start = start->next;
 		curr = start->next;
 		while (*str)
-			line_add(&curr, str++, 1);
+			line_add(&curr, char_dup(str++), 1);
 		start->next = curr;
 	}
 }
