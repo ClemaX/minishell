4ed5d6c85d9759a3a5b372a2fc1b5048108b4248
Clément Hamada <clementhamada@protonmail.com>
2020-02-01 05:26:39 +0100

    string.h not strings.h

--- tests/srcs/error.c
+++ tests/srcs/error.c
@@ -6,13 +6,13 @@
 /*   By: chamada <chamada@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2020/01/26 15:10:19 by chamada      #+#   ##    ##    #+#       */
-/*   Updated: 2020/01/26 15:15:53 by chamada     ###    #+. /#+    ###.fr     */
+/*   Updated: 2020/02/01 05:26:19 by chamada     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include <errno.h>
-#include <strings.h>
+#include <string.h>
 #include <libft.h>
 #include <stdlib.h>
 
