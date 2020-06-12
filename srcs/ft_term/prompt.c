80df8de6e8e8446bb8f14279657307a15aecd6a6
Clément Hamada <clementhamada@protonmail.com>
2020-06-12 19:05:07 +0200

    Fix newline this time

--- srcs/ft_term/prompt.c
+++ srcs/ft_term/prompt.c
@@ -2,12 +2,14 @@
 
 void	term_write_prompt(int status)
 {
-	if (status & TERM_NEWLINE)
-		term_prewrite("\n", 1);
 	if (status & TERM_WAITING)
 		term_prewrite("> ", 2);
 	else
+	{
+		if (status & TERM_NEWLINE)
+			term_prewrite("\n", 1);
 		term_prewrite(TERM_PS1, sizeof(TERM_PS1) - 1);
+	}
 }
 
 int		parse_line(int status)
